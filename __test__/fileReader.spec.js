2cbfc86d95c2804b586e2890786c248efea5e2dc
Aaron <aaronmeade9292@gmail.com>
2018-07-10 20:53:07 -0700

    more linter erros

--- __test__/fileReader.spec.js
+++ __test__/fileReader.spec.js
@@ -4,17 +4,17 @@ const fileReader = require('../lib/fileReader');
 
 describe('File Reader Module', () => {
 
-    it('Should return err when wrong file is read', () => {
-        fileReader([`${__dirname}missing.txt`], (err) => {
-            expect(err).not.toBeUndefined();
-        });
+  it('Should return err when wrong file is read', () => {
+    fileReader([`${__dirname}missing.txt`], (err) => {
+      expect(err).not.toBeUndefined();
     });
+  });
 
-    it('should return one file', () => {
-        fileReader([`${__dirname}burrito.txt`], (err, data) => {
-            let actual = data[0].toString().trim();
-            console.log(actual);
-            expect(actual).toBe('yum burrito');
-        });
+  it('should return one file', () => {
+    fileReader([`${__dirname}burrito.txt`], (err, data) => {
+      let actual = data[0].toString().trim();
+      console.log(actual);
+      expect(actual).toBe('yum burrito');
     });
+  });
 });
