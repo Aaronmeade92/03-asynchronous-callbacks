4ba641a627006c4f7623fb6016674357fa193e19
Aaron <aaronmeade9292@gmail.com>
2018-07-10 20:55:59 -0700

    added a done to my tests

--- __test__/fileReader.spec.js
+++ __test__/fileReader.spec.js
@@ -4,17 +4,19 @@ const fileReader = require('../lib/fileReader');
 
 describe('File Reader Module', () => {
 
-  it('Should return err when wrong file is read', () => {
+  it('Should return err when wrong file is read', (done) => {
     fileReader([`${__dirname}missing.txt`], (err) => {
       expect(err).not.toBeUndefined();
+      done();
     });
   });
 
-  it('should return one file', () => {
+  it('should return one file', (done) => {
     fileReader([`${__dirname}burrito.txt`], (err, data) => {
       let actual = data[0].toString().trim();
       console.log(actual);
       expect(actual).toBe('yum burrito');
+      done();
     });
   });
 });
